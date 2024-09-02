8db9488b5bef38a23edf1284e7da8b0cda9e4aa7
Gabriel Ferraz Duque <83476335+gabrielferrazduque@users.noreply.github.com>
2024-09-02 19:10:42 -0300

    Add files via upload

---
+++ vetoresondenado.c
@@ -0,0 +1,23 @@
+#include <stdio.h>  
+int main() 
+int vetores;  
+/// @brief 
+int vetoresonadedo; 
+int estaondenado; 
+{}  
+{}
+int stable \ int    estaOndenado (int v [] ,int tom)   
+
+for (int i=0 ; i |< tam , ++){ 
+    if (v[i] < v [i-1]){ 
+        return 0  
+
+    }  
+    return 1
+  int stable \   estaOndenado (int v [] ,int tom) 
+for (int i=0 ; i |< tam , ++){ 
+    if (v[i] < v [i+1]){ 
+        return 0 
+    }    
+} 
+return 1 
